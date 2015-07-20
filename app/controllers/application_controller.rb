ed15abf838488b0524077f5d138b4674319ae821
Ian Candy <iancandysemail@gmail.com>
2015-07-20 20:46:07 +0000

    remove some comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 require_relative "../../config/environment"
+require_relative "../models/tweet.rb"
 class ApplicationController < Sinatra::Base
   
+  set :views, "app/views"
+  set :public, "public"
+  
   get "/" do
-    "Hello World"
+    @tweet_1 = Tweet.new("Richard Stamina", "Rold Gold makes me feel like cold mold")
+    erb :tweets
   end
   
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,17 @@
 class Tweet
-  
-  def initialize
+  ALL_TWEETS = [] 
+  attr_accessor :username, :content
+  def initialize(username, content)
+    @username = username
+    @content = content
+    ALL_TWEETS.push(self)
   end
+ 
+  def self.all
+    ALL_TWEETS
+  end
+  
 end
+
+
+
