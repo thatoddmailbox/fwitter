ac9498d00bde645beaf2b611780d25f63bb309c9
Ian Candy <iancandysemail@gmail.com>
2015-07-20 21:17:52 +0000

    update variable name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < Sinatra::Base
   set :public, "public"
   
   get "/" do
-    @tweet_4 = Tweet.new("Richard Stamina", "Rold Gold makes me feel like cold mold")
+    @tweet = Tweet.new("Richard Stamina", "Rold Gold makes me feel like cold mold")
     erb :tweets
   end
 
